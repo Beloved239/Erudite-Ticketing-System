bae57690c1d9936e1147167b4c4d9cbb81a48f82
Beloved239 <adettob@gmail.com>
2023-09-19 15:09:11 +0100

    Erudite Ticketing System Project

---
+++ src/main/java/com/tobi/Erudite_Event_System/EruditeEventSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tobi.Erudite_Event_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EruditeEventSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EruditeEventSystemApplication.class, args);
+	}
+
+}
